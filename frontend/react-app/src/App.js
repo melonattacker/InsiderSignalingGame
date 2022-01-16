7465d488d37fdb4d87c3d610064cd8925d25128d
melonattacker <junkitombob@gmail.com>
2022-01-16 16:10:00 +0900

    ADD: AppBarの追加

---
+++ frontend/react-app/src/App.js
@@ -0,0 +1,16 @@
+import AppBar from './components/AppBar';
+// import UserList from './components/UserList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar />
+      {/* <br/> */}
+      {/* <p class="fs-2"> Scoreboard </p> */}
+      {/* <UserList /> */}
+    </div>
+  );
+}
+
+export default App;
