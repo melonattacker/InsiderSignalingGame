5fb02f239a7053e22792ea5a3174588edae3ac12
melonattacker <junkitombob@gmail.com>
2022-01-23 02:42:52 +0900

    UPDATE: タイトルの変更

--- frontend/react-app/src/components/AppBar.js
+++ frontend/react-app/src/components/AppBar.js
@@ -4,7 +4,7 @@ function AppBar() {
     return (
         <Navbar bg="primary" variant="dark">
             <Container>
-            <Navbar.Brand href="#home">Insider Signaling Game</Navbar.Brand>
+            <Navbar.Brand href="#home">対応策計算デモ</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="/">ホーム</Nav.Link>
             </Nav>
